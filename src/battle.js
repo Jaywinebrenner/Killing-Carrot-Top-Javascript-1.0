ad2918165a0da1c2fe39b3747c98d7cc5f7d4437
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-03-14 19:40:59 -0700

    begin bringing foes into battle class

--- src/battle.js
+++ src/battle.js
@@ -1,44 +1,72 @@
-
-
-let player = {
-	hitPoints: 30,
-	power: 20,
-	armorClass: 5
+let gameStory ={
+	areaOne: 'You have traversed the sprawling metropolis of Laughlandia for weeks on end and have successfully spotted the elusive Prop Tower where the inner sanctum of the Illuminati of Laughter resides. Slinking through garbage strewn alley ways, skillfully avoiding the army of Cybernetic Dane Cooks on partrol, you are greeted with the unusual smell of sauteed garlic clearly eminating from a ramshackled tent strewn together via rusted shopping carts and shredded blue tarps.',
+	areaTwo: ''
 }
 
-let foe = {
-	name: 'Emo Phillips',
-	hitPoints: 30,
-	power: 20,
-	armorClass: 5
+let areaOptions = {
+	areaOneOptionOne: ''
 }
+
+class Player {
+	constructor(name, hitPoints, power, armorClass) {
+		this.name = name;
+		this.hitPoints = hitPoints;
+		this.power = power;
+		this.armorClass = armorClass;
+	}
+}
+
+let player = new Player (null, 30, 20, 10);
+let emoPhilips = new Player ('Emo Phillips',20,10,2);
+let gallagher = new Player ('Gallagher',20,10,2)
+
+
+// let player = {
+// 	hitPoints: 30,
+// 	power: 20,
+// 	armorClass: 5
+// }
+
+// let foe = {
+// 	name: 'Emo Phillips',
+// 	hitPoints: 30,
+// 	power: 20,
+// 	armorClass: 5
+// }
+
+// CONTINUE GAME STORY
+const story = () => {
+	document.getElementById('story-button');
+	printStory();
+}
+
 
 
 // ATTACK
 const attack = () => {
 	let attackButton = document.getElementById('attack-button');
-	let restartButton = document.getElementById('restart-button');
-	let gameMessage = document.getElementById('game-message');
+	let restartButton = document.getElementById('end-battle-button');
+	let gameMessage = document.getElementById('battle-message-text');
 
-	 let playerAttack = determineAttack(player.power - foe.armorClass) ;
-	 foe.hitPoints -= playerAttack;
-	 printToScreen()
+	 let playerAttack = determineAttack(player.power - emoPhilips.armorClass) ;
+	 emoPhilips.hitPoints -= playerAttack;
+	 printBattle()
 
-	 if (isGameOver(foe.hitPoints)){
-		 endGame("You have slain your foe.")
+	 if (isGameOver(emoPhilips.hitPoints)){
+		 endGame("You have slain " + emoPhilips.name)
 		 return;
 	 }
 
 	 attackButton.disabled = true;
-	 gameMessage.innerText = "You attacked and struck your opponent for " + playerAttack +
+	 gameMessage.innerText = "You attacked and crush " + emoPhilips.name + " for " + playerAttack +
 	 " hit points of damage."
 
 	 setTimeout(()=> {
-		 let foeAttack = determineAttack(foe.power - player.armorClass) ;
+		 let foeAttack = determineAttack(emoPhilips.power - player.armorClass) ;
 		 player.hitPoints -= foeAttack;
 		 gameMessage.innerText = "Your vile foe attacks and lands a fantastic blow upon your head for " + foeAttack +
 		 " hit points of damage."
-		 printToScreen();
+		 printBattle();
 
 		 if (isGameOver(player.hitPoints)){
 			 gameMessage.innerText = "You have died."
@@ -50,32 +78,41 @@ const attack = () => {
 	 }, 500)
 }
 
+
+// ENDGAME
 const endGame = (message) => {
-	document.getElementById('game-message').innerText = message;
+	document.getElementById('battle-message-text').innerText = message;
 	document.getElementById('attack-button').hidden = true;
-	document.getElementById('restart-button').hidden = false;
+	document.getElementById('end-battle-button').hidden = false;
 }
 
 const determineAttack = (power) => {
 	return Math.floor(Math.random() * power);
 }
 
+const dice = () => {
+	return Math.floor(Math.random() * 20) +1;
+}
+
+// CHECK IF GAME IS OVER
 const isGameOver = (hitPoints) => {
 	return hitPoints <= 0;
 }
 
+
+
 const restart = () => {
 	let attackButton = 	document.getElementById('attack-button');
 	player.hitPoints = 30;
-	foe.hitPoints = 30;
-	document.getElementById('game-message').innerText = '';
+	emoPhilips.hitPoints = 30;
+	document.getElementById('battle-message-text').innerText = '';
 	attackButton.disabled = false;
 	attackButton.hidden = false;
-	document.getElementById('restart-button').hidden = true;
-		printToScreen();
+	document.getElementById('end-battle-button').hidden = true;
+		printBattle();
 }
 
-const printToScreen = () => {
+const printBattle = () => {
 	document.getElementById('player-hit-points').innerText =
 	player.hitPoints;
 	document.getElementById('player-attack-power').innerText =
@@ -84,9 +121,19 @@ const printToScreen = () => {
 	player.armorClass;
 
 	document.getElementById('foe-name').innerText =
-	foe.name;
+	emoPhilips.name;
 	document.getElementById('foe-hit-points').innerText =
-	foe.hitPoints;
+  emoPhilips.hitPoints;
 }
 
-printToScreen();
+
+
+
+printBattle();
+
+const printStory = (areaNumber) => {
+	document.getElementById('story-message-text').innerText =
+		gameStory.areaOne;
+}
+
+printStory();
